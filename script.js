b24a58779cafcfff0d1b4bddbf54d35e008a8de0
mian <mian@hoermann-logistik.de>
2026-01-14 22:58:44 +0100

    chaged file path to json

---
+++ script.js
@@ -0,0 +1,36 @@
+﻿let index = 0;
+let total = 0;
+
+fetch('images.json')
+    .then(res => res.json())
+    .then(data => {
+        const slides = document.getElementById('slides');
+        total = data.images.length;
+
+        data.images.forEach(src => {
+            const slide = document.createElement('div');
+            slide.className = 'slide';
+
+            const img = document.createElement('img');
+            img.src = src;
+            img.loading = 'lazy';
+
+            slide.appendChild(img);
+            slides.appendChild(slide);
+        });
+    });
+
+function update() {
+    document.getElementById('slides')
+        .style.transform = `translateX(-${index * 100}%)`;
+}
+
+function next() {
+    index = (index + 1) % total;
+    update();
+}
+
+function prev() {
+    index = (index - 1 + total) % total;
+    update();
+}
